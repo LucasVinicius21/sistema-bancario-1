d89ad974c30f29be0d7410f43bbd2207581c34c6
Lucas <naves.vieira@gmail.com>
2024-02-03 18:46:48 -0300

    commit

---
+++ bancario.py
@@ -0,0 +1,42 @@
+menu = """
+    ================ MENU ================
+    [d]\Depositar
+    [s]\Sacar
+    [e]\Extrato
+    [q]\Sair
+    => """
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do deposito: "))
+        
+        if valor > 0:
+            saldo += valor
+            extrato += f"Deposito: R$ {valor:.2f}"
+
+        else:
+            print("Operação falhou! O Valor informado é invalido")
+
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+        
+        excedeu_saldo = valor > saldo
+
+        excedeu_limite = valor > limite
+
+        excedeu_saques = numero_saques  >= LIMITE_SAQUES
+
+        if excedeu_saldo:
+            print("Operação Falhou! Você não tem saldo suficiente. ")
+
+        elif excedeu_limite:
+            print("Operação Falhou! O valor do saque excede o limite")
+
+            
